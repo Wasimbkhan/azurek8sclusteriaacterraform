b033e4720a451476e88c1f6efc84f9d0bd774f81
Wasimbkhan <wasimbkhan1991@outlook.com>
2023-09-30 13:30:05 +0530

    upload all files first time in repo

---
+++ resource.tf
@@ -0,0 +1,33 @@
+resource "azurerm_resource_group" "iaack8s" {
+  name     = "${var.rg_name}_${var.environment}"
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "iaack8" {
+  name = var.aziaack8scluster_name
+  location = azurerm_resource_group.iaack8s.location
+  resource_group_name = azurerm_resource_group.iaack8s.name
+  dns_prefix = var.dns_prefix
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.nodecount
+    vm_size    = "Standard_D2_v2"
+  }
+  linux_profile {
+
+    admin_username = "ubuntu"
+    ssh_key {
+      key_data = 
+    }
+  }
+
+  service_principal {
+    client_id = ""
+    client_secret = ""
+  }
+
+  tags {
+    environment = var.environment
+  }
+}
---
+++ variable.tf
@@ -0,0 +1,35 @@
+variable "rg_name" {
+ default = "AZCICDIAACRG"
+}
+
+variable "environment" {
+    default = "Dev"
+}
+
+variable "location" {
+  default = "West Europe"
+}
+
+variable "aziaack8scluster_name" {
+  default = "aziaack8sterraformtest"
+}
+
+variable "dns_prefix" {
+  default = "azdevk8scluster"
+}
+
+variable "nodecount" {
+  default = 3
+}
+
+variable "client_id" {
+  
+}
+
+variable "client_secret" {
+  
+}
+
+variable "ssh_key" {
+  
+}
