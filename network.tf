ffb0cb9c8a34af2556ae1281f240a2b98b683e33
Wasimbkhan <wasimbkhan1991@outlook.com>
2023-10-18 18:11:56 +0530

    LB_natrule created

--- network.tf
+++ network.tf
@@ -148,18 +148,18 @@ resource "azurerm_lb" "myloadbalancer" {
 #   enable_floating_ip = false
 # }
 
-# resource "azurerm_lb_nat_rule" "natruleforSSH" {
-#   name = "SSH"
-#   resource_group_name = azurerm_resource_group.kubertera.name
-#   loadbalancer_id = azurerm_lb.myloadbalancer.id
-#   protocol = "Tcp"
-#   frontend_port_start = 22
-#   frontend_port_end = 23
-#   backend_port = 22
-#   frontend_ip_configuration_name = "LBPublicIP"
-#   backend_address_pool_id = azurerm_lb_backend_address_pool.lbbackendpool.id
-#   enable_floating_ip = false
-# }
+resource "azurerm_lb_nat_rule" "natruleforSSH" {
+  name = "SSH"
+  resource_group_name = azurerm_resource_group.kubertera.name
+  loadbalancer_id = azurerm_lb.myloadbalancer.id
+  protocol = "Tcp"
+  frontend_port_start = 22
+  frontend_port_end = 22
+  backend_port = 22
+  frontend_ip_configuration_name = "LBPublicIP"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.lbbackendpool.id
+  enable_floating_ip = false
+}
 
 ### Create LB rule ###
 
