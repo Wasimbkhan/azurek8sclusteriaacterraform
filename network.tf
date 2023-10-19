a6178ce38ff93acbb15f5de82249cffdc017e62d
Wasimbkhan <wasimbkhan1991@outlook.com>
2023-10-19 16:32:16 +0530

    creating one more subnet for AZFW

--- network.tf
+++ network.tf
@@ -36,7 +36,7 @@ resource "azurerm_subnet" "AzureFirewallSubnet" {
   name = "AzureFirewallSubnet"
   resource_group_name = azurerm_resource_group.kubertera.name
   virtual_network_name = azurerm_virtual_network.myazvnet.name
-  address_prefixes = ["10.1.1.0/26"]
+  address_prefixes = ["10.1.4.0/26"]
   
 }
 
