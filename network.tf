893f7d7f795005602cd825f2009e02d5757f6c1a
Wasimbkhan <wasimbkhan1991@outlook.com>
2023-10-19 15:58:56 +0530

    updated lb SKU

--- network.tf
+++ network.tf
@@ -113,7 +113,7 @@ resource "azurerm_lb" "myloadbalancer" {
   name = "myloadbalancer"
   resource_group_name = azurerm_resource_group.kubertera.name
   location = azurerm_resource_group.kubertera.location
-  sku = "basic"
+  sku = "Basic"
 
   frontend_ip_configuration {
     name = "LBPublicIP"
